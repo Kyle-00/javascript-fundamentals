073b2ddaa3ba529a79da6d7119f5030bc978e977
Kyle-00 <kylekinyua001@gmail.com>
2026-05-27 13:06:06 +0300

    Add initial script.js file with variable declaration and comments

---
+++ script.js
@@ -0,0 +1,12 @@
+// This is a single line comment
+/*
+This is a multi line comment
+*/
+
+//Javascript Variables
+/*
+A named container used to store data that can be used later.
+*/
+// Declaration
+let name = "Kyle"
+console.log(name);
