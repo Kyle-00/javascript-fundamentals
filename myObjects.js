be5108c98b32d95484ff5dcaad3c1f7acc6eb56d
Kyle-00 <kylekinyua001@gmail.com>
2026-06-02 13:11:57 +0300

    Add myObjects.js to define a user object and demonstrate object key retrieval

---
+++ myObjects.js
@@ -0,0 +1,12 @@
+const user ={
+    name:"Jay",
+    age: 21,
+    status: "active",
+    isLoggedIn: false,
+}
+
+
+let myKeys= Object.keys(user)
+console.log(Object.keys(user))
+console.log(`I am ${user.name}, I am ${user.age} years old, I am an ${user.status} member in the science club`)
+
