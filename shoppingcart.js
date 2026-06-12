6862299fb84a33a20aa0126bb6eee255f1b2dc49
Kyle-00 <kylekinyua001@gmail.com>
2026-06-12 12:46:56 +0300

    Add shopping cart functions for discount calculation and cart summary

---
+++ shoppingcart.js
@@ -0,0 +1,16 @@
+function calculateDiscount(totalAmount, couponCode){
+    if (totalAmount < 0) return 0;
+
+    if (couponCode === "SAVE10"){
+        return 0.10;
+    }
+    return 0;
+}
+
+function cartSummary(items){
+    if (items.length=== 0) return "Your cart is empty";
+    if (items.length===1) return "You have 1 item in your cart";
+    return `You have ${items.length} items in your cart`
+}
+
+module.exports = { calculateDiscount, cartSummary };
