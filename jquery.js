2e31d5393401468fa716e4f50e5c6afb7f3e0e16
Kyle-00 <kylekinyua001@gmail.com>
2026-06-17 14:02:46 +0300

    Add fetch functionality to load user profile data on button click

--- jquery.js
+++ jquery.js
@@ -20,3 +20,29 @@ $(document).ready(function(){
         });
     })
 })    
+
+$('#loadProfileBtn').on('click', function() {
+            $('#fetchMessage').html('<i class="fa-solid fa-circle-notch fa-spin mr-1"></i> Fetching data...')
+                .removeClass().addClass('mt-4 text-sm font-medium text-center text-slate-500');
+            
+            var dataUrl = 'https://jsonplaceholder.typicode.com/users/2';
+            
+            fetchPayload(dataUrl, 
+                function(data) {
+                    $('#displayUsername').text(data.username);
+                    $('#displayEmail').text(data.email);
+                    
+                    $('#userDataDisplay').removeClass('hidden').addClass('block animate-fade-in');
+                    
+                  
+                    $('#fetchMessage').html('<i class="fa-solid fa-circle-check mr-1"></i> Data rendered successfully!')
+                        .removeClass().addClass('mt-4 text-sm font-medium text-center text-emerald-600');
+                },
+                function(xhr, status, error) {
+                  
+                    $('#fetchMessage').html('<i class="fa-solid fa-circle-exclamation mr-1"></i> Failed to fetch data.')
+                        .removeClass().addClass('mt-4 text-sm font-medium text-center text-rose-600');
+                    console.error("Fetch error: ", error);
+                }
+            );
+        });
